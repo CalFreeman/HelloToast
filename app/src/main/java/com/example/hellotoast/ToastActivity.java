0068ca7648c3a57eff2723a69344c70f141acc63
ctfdx2 <ctfdx2@umsl.edu>
2020-03-19 00:04:57 -0500

    Modify the Toast button so that it launches a new Activity to display the word Hello! and the current count, as shown below.

---
+++ app/src/main/java/com/example/hellotoast/ToastActivity.java
@@ -0,0 +1,13 @@
+package com.example.hellotoast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ToastActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_toast);
+    }
+}
